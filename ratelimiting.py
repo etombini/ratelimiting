6c64cc44fd4f3716c3079fd1bb0ed385c0c3c6dc
Elvis <elvis@mapom.me>
2014-03-29 00:42:01 +0100

    first commit

--- ratelimiting.py
+++ ratelimiting.py
@@ -1,29 +1,31 @@
 
 
 import time
-
-# TODO: Rename "rate" & "per"
+from numbers import Number
 
 
 class RateLimiting(object):
 
-    def __init__(self, rate=1, per=1):
-#        if type(rate) is not int:
-#            raise AttributeError('rate must be an integer')
-#        if type(per) is not int:
-#            raise AttributeError('per must be an integer')
-        self.rate = float(rate)
-        self.per = float(per)
-        self.allowance = self.rate
+    def __init__(self, hit=1.0, period=1.0):
+        if not isinstance(hit, Number):
+            raise TypeError('hit must be a number')
+        if not isinstance(period, Number):
+            raise TypeError('period must be a number')
+        if hit < 1:
+            raise ValueError('hit must be >= 1 or allowance is never reached')
+        self.hit = float(hit)
+        self.period = float(period)
+        self.allowance = self.hit
         self.last_check = time.time()
 
     def allow(self):
         current = time.time()
         time_passed = current - self.last_check
         self.last_check = current
-        self.allowance = self.allowance + time_passed * (self.rate / self.per)
-        if self.allowance > self.rate:
-            self.allowance = self.rate
+        self.allowance = self.allowance + \
+            time_passed * (self.hit / self.period)
+        if self.allowance > self.hit:
+            self.allowance = self.hit
         if self.allowance < 1:
             return False
         else:
@@ -31,8 +33,8 @@ class RateLimiting(object):
             return True
 
 
-def test():
-    rl = RateLimiting(0.1, 1)
+def test(hit, period):
+    rl = RateLimiting(hit, period)
     for i in range(0, 200):
         print('allowance: ' + str(rl.allowance))
         if rl.allow():
@@ -40,5 +42,3 @@ def test():
         else:
             print('Deny ' + str(i))
             time.sleep(1)
-
-
